1d454c9c6c610b77c040b932d4cf031cc26bf630
gtretow <guilherme.tretow@gmail.com>
2022-11-14 17:56:03 -0300

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :students
   devise_for :professors
+
+  devise_scope :students do  
+    get '/students/sign_out' => 'devise/sessions#destroy'   
+  end
+
+  devise_scope :professors do  
+    get '/professors/sign_out' => 'devise/sessions#destroy'   
+  end
+
+
   resources :university_subjects
   root 'home#index'
 end
